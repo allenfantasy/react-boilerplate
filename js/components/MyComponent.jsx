b89c415b9dcd784f924e218c2237a385bef7debd
Allen Wu <allenwu1018@gmail.com>
2015-11-01 15:30:09 +0800

    React bump to 0.14; use webpack & babel-loader instead of jsxc; change dir conventions

---
+++ js/components/MyComponent.jsx
@@ -0,0 +1,11 @@
+'use strict';
+
+import React from 'react';
+
+let MyComponent = React.createClass({
+  render: function() {
+    return <span className="MyComponent">Hello, MyComponent!</span>;
+  }
+});
+
+module.exports = MyComponent;
